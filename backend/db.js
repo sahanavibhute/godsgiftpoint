219acc884d62d29d24080c7ea6a849b628aa74f2
sahanavibhute <sahanavibhute120@gmail.com>
2026-07-10 19:37:47 +0530

    Support MONGO_URI in db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/gods-gift-fitness';
+const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://localhost:27017/gods-gift-fitness';
 
 export const connectDB = async () => {
   try {
